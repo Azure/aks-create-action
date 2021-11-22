bf9940a2ff76043846612bff6ddeb8d862601e85
mahines <marcus.hines@microsoft.com>
2021-11-22 14:04:06 -0500

    attach-acr

--- dev/main.tf
+++ dev/main.tf
@@ -61,3 +61,9 @@ variable "cluster_name" {
   type = string
 }
 
+# add the role to the identity the kubernetes cluster was assigned
+resource "azurerm_role_assignment" "kubweb_to_acr" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.kubweb.kubelet_identity[0].object_id
+}
