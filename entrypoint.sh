f0297cb699c97b4ea508b3764b3b547268fa58bf
mahines <marcus.hines@microsoft.com>
2021-12-08 09:32:37 -0500

    adding full pulumi path

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ export TF_VAR_create_acr=$INPUT_CREATE_ACR
 export TF_IN_AUTOMATION=true
 
 ## Use TF based on cluster size variable
-##cd /action/$INPUT_CLUSTER_SIZE
+cd /action/$INPUT_CLUSTER_SIZE
 
 echo "*******************"
 echo "Running init"
@@ -28,22 +28,22 @@ echo "*******************"
 if [ $INPUT_USE_PULUMI = "true" ]; then
  echo "Using Pulumi"
 
- pulumi stack select dev --create
- pulumi config set azure:clientId ${ARM_CLIENT_ID}
- pulumi config set azure:clientSecret ${ARM_CLIENT_SECRET} --secret
- pulumi config set azure:tenantId ${ARM_TENANT_ID}
- pulumi config set azure:subscriptionId ${ARM_SUBSCRIPTION_ID}
+ /pulumi/bin/pulumi stack select dev --create
+ /pulumi/bin/pulumi config set azure:clientId ${ARM_CLIENT_ID}
+ /pulumi/bin/pulumi config set azure:clientSecret ${ARM_CLIENT_SECRET} --secret
+ /pulumi/bin/pulumi config set azure:tenantId ${ARM_TENANT_ID}
+ /pulumi/bin/pulumi config set azure:subscriptionId ${ARM_SUBSCRIPTION_ID}
 
   if [ $INPUT_ACTION_TYPE = "destroy" ]; then
       echo "*******************"
       echo "Running destroy"
       echo "*******************"
-      pulumi destroy -force
+      /pulumi/bin/pulumi destroy -force
   else
       echo "*******************"
       echo "Running apply"
       echo "*******************"
-      pulumi up --yes
+      /pulumi/bin/pulumi up --yes
   fi
 
   exit 1
