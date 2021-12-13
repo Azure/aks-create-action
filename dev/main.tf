763df6c576c2086f33d351c42ee13fd2ad5124e6
mahines <marcus.hines@microsoft.com>
2021-12-13 09:28:35 -0500

    trying a different approach to attach acr

--- dev/main.tf
+++ dev/main.tf
@@ -63,7 +63,8 @@ variable "cluster_name" {
 
 # add the role to the identity the kubernetes cluster was assigned
 resource "azurerm_role_assignment" "kubweb_to_acr" {
-  scope                = azurerm_container_registry.acr[0].acr.id
+  enable_attach_acr = true
+  acr_id            = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.cluster.kubelet_identity[*].object_id
+
 }
