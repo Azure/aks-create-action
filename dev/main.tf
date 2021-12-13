25b4dd4998680bd5e09bb40c1a683267aef9fbfe
mahines <marcus.hines@microsoft.com>
2021-12-13 10:35:05 -0500

    adding depends on for identity.

--- dev/main.tf
+++ dev/main.tf
@@ -63,6 +63,7 @@ variable "cluster_name" {
 
 # add the role to the identity the kubernetes cluster was assigned
 resource "azurerm_role_assignment" "kubweb_to_acr" {
+  depends_on           = [azurerm_kubernetes_cluster.cluster.kubelet_identity]
   scope                = azurerm_container_registry.acr[0].id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_kubernetes_cluster.cluster.kubelet_identity[*].object_id
